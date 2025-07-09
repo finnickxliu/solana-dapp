e49634720095ff71f9ff88480190d8777e2cde03
finnickxliu <dhenynovelius999@gmail.com>
2025-07-09 20:08:45 +0700

    Create WalletConnectionProvider.js

---
+++ src/components/WalletConnectionProvider.js
@@ -0,0 +1,30 @@
+import React, { useMemo } from 'react';
+import {
+  ConnectionProvider,
+  WalletProvider
+} from '@solana/wallet-adapter-react';
+import {
+  PhantomWalletAdapter
+} from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+export const WalletConnectionProvider = ({ children }) => {
+  const endpoint = clusterApiUrl('devnet');
+
+  const wallets = useMemo(() => [
+    new PhantomWalletAdapter()
+  ], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
